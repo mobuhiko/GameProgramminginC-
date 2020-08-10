406f1ff50d8b492da846cea7fda1ec8baf5125cd
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-08-10 17:28:53 +0900

    Create Game.cpp

---
+++ Chapter001/Game.cpp
@@ -0,0 +1,51 @@
+#include <game.h>
+
+void Game::Initialze()
+{
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    if(sdlResult != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError);
+        return false;
+    }
+
+    mWindow =
+    SDL_CreteWindow(
+                    // window title
+                    "Game Programming in C++ (Chapter 1)",
+                    // top left x-coordinate of window
+                    100,
+                    // top left y-coordinate of window
+                    100,
+                    // width of window
+                    1024,
+                    // height of window
+                    768,
+                    // flags (0 for no flags set)
+                    0
+                    );
+
+    if(!mWindow)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetErrot);
+        return false;
+    }
+
+    return true;
+}
+
+void Game::Shutdown()
+{
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+void Game::RunLoop()
+{
+    while(mIsRunning)
+    {
+        ProcessInput();
+        UpdatefGame();
+        GenerateOutput();
+    }
+}
